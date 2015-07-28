217f3e154cf14452af3c01cc1d7ce5e148cab6f2
amritadheesh <amrit.adheesh@globallogic.com>
2015-07-28 19:28:42 +0530

    commit4@27thJul

--- src/main/java/SendEmail.java
+++ src/main/java/SendEmail.java
@@ -1,8 +1,7 @@
-package JavaMail.src.test.java.JavaMail;
 
-
-
-import java.io.Console;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 import java.util.Scanner;
 
@@ -15,70 +14,90 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 public class SendEmail {
-   public static void main(String[] args) {
-      // Recipient's email ID needs to be mentioned.
-	   Scanner in =new Scanner(System.in);
-	   System.out.println("Enter recipient mail id");
-      String to = in.nextLine();
 
-      // Sender's email ID needs to be mentioned
-      System.out.println("Enter sender mail id");
-      String from = in.nextLine();
-      System.out.println("Enter sender username");
-      final String username = in.nextLine();//change accordingly
-     /* Console console=System.console();
-      System.out.println("Enter password");
-      char[] pass=console.readPassword();
-      final String password = String.valueOf(pass);//change accordingly
-      
-      
-*/
-      
-      System.out.println("Enter password");
-      final String password = in.nextLine();//change accordingly
-      // Assuming you are sending email through relay.jangosmtp.net
-      String host = "smtp.gmail.com";
+	public boolean sendMail(String to) {
+		
+		
+		Boolean bool = false;
+		InputStream input=null;
+		final String from, username, password, host, port;
+		Properties props = new Properties();
 
-      Properties props = new Properties();
-      props.put("mail.smtp.auth", "true");
-      props.put("mail.smtp.starttls.enable", "true");
-      props.put("mail.smtp.host", host);
-      props.put("mail.smtp.port", "587");
-     // props.put("mail.smtp.debug", "true");
+		try {
+			
+			//loading config.property file
+			input = new FileInputStream("src/main/resources/config.properties");
+			props.load(input);
+			
+			//reading values from config file
+			from = props.getProperty("sender");
+			username = props.getProperty("username");
+			password = props.getProperty("password");
+			host = props.getProperty("host");
+			port = props.getProperty("port");
+			
+			//other smtp properties set up
+			
+			props.put("mail.smtp.auth", "true");
+			props.put("mail.smtp.starttls.enable", "true");
+			props.put("mail.smtp.host", host);
+			props.put("mail.smtp.port", port);
+			Session session = Session.getInstance(props,
+					new javax.mail.Authenticator() {
+						protected PasswordAuthentication getPasswordAuthentication() {
+							return new PasswordAuthentication(username, password);
+						}
+					});
 
-      // Get the Session object.
-      Session session = Session.getInstance(props,
-         new javax.mail.Authenticator() {
-            protected PasswordAuthentication getPasswordAuthentication() {
-               return new PasswordAuthentication(username, password);
-	   }
-         });
+			try {
+				// Create a default MimeMessage object.
+				Message message = new MimeMessage(session);
 
-      try {
-	   // Create a default MimeMessage object.
-	   Message message = new MimeMessage(session);
-	
-	   // Set From: header field of the header.
-	   message.setFrom(new InternetAddress(from));
-	
-	   // Set To: header field of the header.
-	   message.setRecipients(Message.RecipientType.TO,
-               InternetAddress.parse(to));
-	
-	   // Set Subject: header field
-	   message.setSubject("Testing Subject");
-	
-	   // Now set the actual message
-	   message.setText("Hello, this is sample for to check send " +
-		"email using JavaMailAPI ");
+				// Set From: header field of the header.
+				message.setFrom(new InternetAddress(from));
 
-	   // Send message
-	   Transport.send(message);
+				// Set To: header field of the header.
+				message.setRecipients(Message.RecipientType.TO,
+						InternetAddress.parse(to));
 
-	   System.out.println("Sent message successfully....");
+				// Set Subject: header field
+				message.setSubject("Testing Subject");
 
-      } catch (MessagingException e) {
-         throw new RuntimeException(e);
-      }
-   }
+				// Now set the actual message
+				message.setText("Hi, this is sample for to check send "
+						+ "email using JavaMailAPI ");
+
+				// Send message
+				Transport.send(message);
+
+				System.out.println("Sent message successfully....");
+				
+				bool=true;
+
+			} catch (MessagingException e) {
+				throw new RuntimeException(e);
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		// props.put("mail.smtp.debug", "true");
+
+		// Get the Session object.
+		return bool;
+		
+	}
+
+	public static void main(String[] args) {
+		// Recipient's email ID needs to be mentioned.
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter recipient mail id");
+		String to = in.nextLine();
+		SendEmail mail = new SendEmail();
+		mail.sendMail(to);
+
+		// Sender's email ID needs to be mentioned
+
+	}
 }
